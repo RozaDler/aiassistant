da7e8ef22ff549e0bd2bc17d51b8a5981dff9876
Roza <rozadler.rd@gmail.com>
2025-07-11 22:55:28 +0300

    setup CORS and exception handlers

---
+++ server/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
+from middlewares.exception_handlers import catch_exception_middleware
+
+#cors middleweare because backend and front end running on different port
+
+app=FastAPI(title="Ai Assistant", description="API for AI Assistant Chatbot")
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# middleweare exception handlers
+app.middleware("http")(catch_exception_middleware)
+
+# routers
+
+# 1. upload pdf documents
+
+# 2. asking query
